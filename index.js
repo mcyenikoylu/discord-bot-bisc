1a98fd32381c17938dae7a944a35b6b2063384c1
Mehmet Cenk Yenikoylu <mcyenikoylu@gmail.com>
2020-10-17 22:38:15 +0300

    scop free

--- index.js
+++ index.js
@@ -5,49 +5,49 @@ const { prefix, token } = require('./config.json');
 //const creds = require(process.env.GOOGLE_CREDENTIALS);
 //const creds = process.env.GOOGLE_CREDENTIALS;
 
-const clientGoogle = new google.auth.JWT(
-  process.env.GOOGLE_CLIENT_EMAIL,//creds.client_email,
-  null,
-  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),//creds.private_key,
-  ['https://www.googleapis.com/auth/spreadsheets']
-);
+let n;
 
-clientGoogle.authorize(function(err,tokens){
-  if(err){
-      console.log(err);
-      //return;
-  } else {
-      console.log('Connect mcy');
-      gsrun(clientGoogle);
-  }
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-async function gsrun(cl){
-  const gsapi = google.sheets({version:'v4', auth: cl});
-  const doc = {
-      spreadsheetId: '1MhXCEKaqYbyHiDr8hmLoqShgp2NhNwQSXHK_eZx8uBk',//process.env.SPREADSHEETID,
-      range: 'BISCBot!A1:H'
-  }
-  let req = await gsapi.spreadsheets.values.get(doc);
-  let rows = req.data.values;
-  let n = rows.length+1;
+client.on('message', message => {
+  //if (message.content.startsWith(`${prefix}basvurdum`)) {
 
-  client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  });
+    const clientGoogle = new google.auth.JWT(
+      process.env.GOOGLE_CLIENT_EMAIL,//creds.client_email,
+      null,
+      process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),//creds.private_key,
+      ['https://www.googleapis.com/auth/spreadsheets']
+    );
+    
+    clientGoogle.authorize(function(err,tokens){
+      if(err){
+          console.log(err);
+          //return;
+      } else {
+          console.log('Connect mcy');
+          gsrun(clientGoogle);
+      }
+    });
 
-  client.on('message', message => {
-    if (message.content.startsWith(`${prefix}basvurdum`)) {
-
-      
-
+    async function gsrun(cl){
+      const gsapi = google.sheets({version:'v4', auth: cl});
+      const doc = {
+          spreadsheetId: '1MhXCEKaqYbyHiDr8hmLoqShgp2NhNwQSXHK_eZx8uBk',//process.env.SPREADSHEETID,
+          range: 'BISCBot!A1:H'
+      }
+      let req = await gsapi.spreadsheets.values.get(doc);
+      let rows = req.data.values;
+      let n = rows.length+1;
+    
       let chan = message.channel.id;
       if(chan==='763376896284426280'){
         let mesajorj = message.content;
-        let mesajrep = mesajorj.replace(`${prefix}ping`,'');
-        let chan = message.channel.id; 
+        //let mesajrep = mesajorj.replace(`${prefix}ping`,'');
+        //let chan = message.channel.id; 
         let disc = message.author.discriminator;
-        let time = message.createdAt.toString();
+        //let time = message.createdAt.toString();
         let timestamp = message.createdTimestamp.toString();
         let uname = message.author.username;
         let uid = message.author.id;
@@ -62,9 +62,21 @@ async function gsrun(cl){
         gsapi.spreadsheets.values.update(docUpdate);
     
       }
-    }
-  });
 
-}
+    
+    
+    
+    }
+
+
+
+
+    
+  //}
+});
+
+
+
+
 
 client.login(process.env.BOT_TOKEN);
